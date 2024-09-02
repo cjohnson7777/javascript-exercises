19320d4689397bcae0116392dd795599dc66de3a
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-09-02 14:48:01 -0500

    06-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let celsius = (tempInFahrenheit - 32)*(5/9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempinCelsius) {
+  let fahrenheit = (tempinCelsius * (9/5)) + 32
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
@@ -9,3 +13,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//(32°F − 32) × 5/9 = 0°C
+//(0°C × 9/5) + 32 = 32°F
