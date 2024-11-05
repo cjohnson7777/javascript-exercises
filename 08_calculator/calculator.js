9f5d27d94c98b7b5a174081ca60046d3c25f7974
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-05 16:42:33 -0600

    08 - 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current)
 
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return base**expo
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n <= 1){
+    return 1
+  }
+  return(factorial(n-1) * n)
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let validchars = 'qwertyuioplkjhgfdsazxcvbnm1234567890'
 
+    let plainString = word.toLowerCase().split('').filter((char) => validchars.includes(char)).join('')
+
+    let reversed = plainString.split('').reverse().join('')
+
+    return reversed === plainString
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
